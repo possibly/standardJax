b4fffdf522e31e05c8771152fc7e8123f4b32e83
Tyler Brothers <tylerbrothers1@gmail.com>
2015-09-22 03:19:54 -0700

    Loop logging. Better interpretPlayer.

--- main.js
+++ main.js
@@ -11,32 +11,45 @@ var world = new World(emitter);
 
 var programsDir = './programs/';
 
+//loop -> world -> interpreter -> runner -> interpreter -> interpreter ->  world -> world -> interpreter
 function loop(files){
   files.forEach(function(file){
-    var player = world.find(file)
+    var player = world.find(file)[0];
     var state = interpreter.interpretPlayer(player);
-    runner.run(file, state);
+    runner.run(getFullPath(file), state);
   });
 }
 
-function getFullPath(files){
-  var fullPathFiles = files.map(function(file){
-    return programsDir.concat(file);
-  });
-  return fullPathFiles;
+function getFullPath(file){
+  return programsDir.concat(file);
 }
 
+// runner -> world
 function setupAndStartLoop(err, files){
-  var fullPathFiles = getFullPath(files);
-  // console.log(fullPathFiles);
+  var fullPathFiles = files.map(getFullPath);
   fullPathFiles.forEach(runner.setup, runner);
-  loop(fullPathFiles);
+  loop(files);
 }
 
+//output processing
+emitter.on('playerTurnStart', function(player){
+  console.log("It is "+player+"'s turn.");
+});
+emitter.on('turnEnd', function(player){
+  console.log('The player now looks like this:');
+  console.log(interpreter.interpretPlayer(player));
+  console.log(player.stats.name+"'s turn is now over.")
+  console.log('-------------------------------------');
+});
+
+emitter.on('playerAction', function(player,action){
+  console.log(player+' tried to '+JSON.stringify(action));
+});
+
+//game flow
 emitter.on('playerSetup', world.addPlayer.bind(world));
 emitter.on('playerTurnStart', interpreter.interpretAction.bind(interpreter));
 emitter.on('playerAction', world.act.bind(world));
 emitter.on('move', world.move.bind(world));
-emitter.on('turnEnd', interpreter.display.bind(interpreter));
 
 fs.readdir(programsDir, setupAndStartLoop);
