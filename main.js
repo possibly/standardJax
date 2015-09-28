fa38d628ec8ea867c73727ff485dbf856eae0fbd
Tyler Brothers <tylerbrothers1@gmail.com>
2015-09-28 00:03:51 -0700

    removed Runner

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 var fs = require('fs');
 var EventEmitter = require('events').EventEmitter;
-var Runner = require('./lib/Runner.js');
 var Interpreter = require('./lib/Interpreter.js');
 var World = require('./lib/World.js');
 
+var exec = require('child_process').execSync;
+var spawn = require('child_process').spawnSync;
+
 var emitter = new EventEmitter();
-var runner = new Runner(emitter);
 var interpreter = new Interpreter(emitter);
 var world = new World(emitter);
 
 var programsDir = './programs/';
 
-//loop -> world -> interpreter -> runner -> interpreter -> interpreter ->  world -> world -> interpreter
 function loop(files){
   files.forEach(function(file){
     var player = world.find(file)[0];
     var state = interpreter.interpretPlayer(player);
-    var playerOutput = runner.run(getFullPath(file), state);
+    var playerOutput = runProgram(getFullPath(file), state);
     var playerAction = interpreter.interpretAction(file, playerOutput);
 
     emitter.emit('playerTurnStart', file, playerOutput);
     emitter.emit('playerAction', file, playerAction);
   });
+}
+
+function runProgram(path, input){
+  var player = spawn(path, {input: input});
+  var stdout = ""+player.stdout
+  return stdout;
 }
 
 function getFullPath(file){
@@ -31,7 +37,9 @@ function getFullPath(file){
 // runner -> world
 function setupAndStartLoop(err, files){
   var fullPathFiles = files.map(getFullPath);
-  fullPathFiles.forEach(runner.setup);
+  fullPathFiles.forEach( function(path){
+    exec('chmod +x '+path);
+  });
 
   files.forEach( function(file){
     emitter.emit('playerSetup', file)
@@ -69,7 +77,7 @@ emitter.on('playerCommunicate', function playerCommunicate(currentPlayerAction, 
   var targetPlayerName = currentPlayerAction[1];
   var targetPlayerRunPath = getFullPath(targetPlayerName);
   var state = interpreter.stealOrShare(currentPlayer.name);
-  var targetPlayerOutput = runner.run(targetPlayerRunPath, state);
+  var targetPlayerOutput = runProgram(targetPlayerRunPath, state);
   var targetPlayer = world.find(targetPlayerName);
   var targetPlayerAction = interpreter.interpretAction(targetPlayer.name, targetPlayerOutput);
   emitter.emit('playerCommunicateAction', currentPlayerAction, currentPlayer, targetPlayerAction, targetPlayer);
