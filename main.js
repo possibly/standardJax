fc65853a2aff37177f9ef43774235fe1907392a9
Tyler Brothers <tylerbrothers1@gmail.com>
2015-10-01 03:15:19 -0700

    WIP

--- main.js
+++ main.js
@@ -1,87 +1,116 @@
-var fs = require('fs');
-var EventEmitter = require('events').EventEmitter;
-var Interpreter = require('./lib/Interpreter.js');
+//my modules
 var World = require('./lib/World.js');
+var world = new World();
 
-var exec = require('child_process').execSync;
-var spawn = require('child_process').spawnSync;
+//node modules
+var fs = require('fs');
+var exec = require('child_process').exec;
+var stream = require('stream');
 
-var emitter = new EventEmitter();
-var interpreter = new Interpreter(emitter);
-var world = new World(emitter);
+//third party modules
+var concat = require('concat-stream');
+var Rounds = require('rounds-emitter');
+var rounds = new Rounds();
+var readdir = require('readdir-stream');
+var through2 = require('through2');
+var fromArray = require('stream-from-array');
+var split = require('split');
 
 var programsDir = './programs/';
 
-function loop(files){
-  files.forEach(function(file){
-    var player = world.find(file)[0];
-    var state = interpreter.interpretPlayer(player);
-    var playerOutput = runProgram(getFullPath(file), state);
-    var playerAction = interpreter.interpretAction(file, playerOutput);
 
-    emitter.emit('playerTurnStart', file, playerOutput);
-    emitter.emit('playerAction', file, playerAction);
-  });
-}
 
-function runProgram(path, input){
-  var player = spawn(path, {input: input});
-  var stdout = ""+player.stdout
-  return stdout;
-}
+// function loop(files){
+//   files.forEach(function(file){
+//     var player = world.find(file)[0];
+//     var state = interpreter.interpretPlayer(player);
+//     var playerOutput = runProgram(getFullPath(file), state);
+//     var playerAction = interpreter.interpretAction(file, playerOutput);
 
-function getFullPath(file){
-  return programsDir.concat(file);
-}
+//     emitter.emit('playerTurnStart', file, playerOutput);
+//     emitter.emit('playerAction', file, playerAction);
+//   });
+// }
 
-// runner -> world
-function setupAndStartLoop(err, files){
-  var fullPathFiles = files.map(getFullPath);
-  fullPathFiles.forEach( function(path){
-    exec('chmod +x '+path);
-  });
-
-  files.forEach( function(file){
-    emitter.emit('playerSetup', file)
-  });
-
-  loop(files);
-}
+// function runProgram(path, input){
+//   var player = spawn(path, {input: input});
+//   var stdout = ""+player.stdout
+//   return stdout;
+// }
 
 //output processing
-emitter.on('playerSetup', function(file){
-  console.log(file+' is now setup.');
-});
-emitter.on('playerTurnStart', function(player){
-  console.log("It is "+player+"'s turn.");
-});
-emitter.on('turnEnd', function(player, success){
-  console.log('The player '+success);
-  console.log('The player model now looks like this:');
-  console.log(interpreter.interpretPlayer(player));
-  console.log(interpreter.getPlayerName(player)+"'s turn is now over.")
-  console.log('-------------------------------------');
-});
+// emitter.on('playerSetup', function(file){
+//   console.log(file+' is now setup.');
+// });
+// emitter.on('playerTurnStart', function(player){
+//   console.log("It is "+player+"'s turn.");
+// });
+// emitter.on('turnEnd', function(player, success){
+//   console.log('The player '+success);
+//   console.log('The player model now looks like this:');
+//   console.log(interpreter.interpretPlayer(player));
+//   console.log(interpreter.getPlayerName(player)+"'s turn is now over.")
+//   console.log('-------------------------------------');
+// });
 
-emitter.on('playerAction', function(player,action){
-  console.log(player+' tried to '+action);
-});
+// emitter.on('playerAction', function(player,action){
+//   console.log(player+' tried to '+action);
+// });
 
 //game flow
-emitter.on('playerSetup', world.addPlayer.bind(world));
-emitter.on('playerAction', world.act.bind(world));
+// emitter.on('playerSetup', world.addPlayer.bind(world));
+// emitter.on('playerAction', world.act.bind(world));
 
 //player - world interaction
-emitter.on('move', world.move.bind(world));
-emitter.on('playerCommunicate', function playerCommunicate(currentPlayerAction, currentPlayer){
-  var targetPlayerName = currentPlayerAction[1];
-  var targetPlayerRunPath = getFullPath(targetPlayerName);
-  var state = interpreter.stealOrShare(currentPlayer.name);
-  var targetPlayerOutput = runProgram(targetPlayerRunPath, state);
-  var targetPlayer = world.find(targetPlayerName);
-  var targetPlayerAction = interpreter.interpretAction(targetPlayer.name, targetPlayerOutput);
-  emitter.emit('playerCommunicateAction', currentPlayerAction, currentPlayer, targetPlayerAction, targetPlayer);
-});
-emitter.on('playerCommunicateAction', world.playerCommunicateAction.bind(world));
+// emitter.on('move', world.move.bind(world));
+// emitter.on('playerCommunicate', function playerCommunicate(currentPlayerAction, currentPlayer){
+//   var targetPlayerName = currentPlayerAction[1];
+//   var targetPlayerRunPath = getFullPath(targetPlayerName);
+//   var state = interpreter.stealOrShare(currentPlayer.name);
+//   var targetPlayerOutput = runProgram(targetPlayerRunPath, state);
+//   var targetPlayer = world.find(targetPlayerName);
+//   var targetPlayerAction = interpreter.interpretAction(targetPlayer.name, targetPlayerOutput);
+//   emitter.emit('playerCommunicateAction', currentPlayerAction, currentPlayer, targetPlayerAction, targetPlayer);
+// });
+// emitter.on('playerCommunicateAction', world.playerCommunicateAction.bind(world));
 
-fs.readdir(programsDir, setupAndStartLoop);
+function getPlayerName(path){
+  return path.split('/')[path.split('/').length-1];
+}
+
+rounds.on('setup', function(){
+  readdir(programsDir)
+    .pipe(through2.obj(function preparePrograms(chunk,_,cb){
+      if (chunk.path === programsDir){ return cb(); }
+      chunk = './'+chunk.path;
+      exec('chmod +x '+chunk);
+      var playerName = getPlayerName(chunk);
+      world.addPlayer(playerName);
+      this.push(chunk+'\n');
+      cb();
+    }))
+    .pipe(concat(function(data){ 
+      rounds.next(data) 
+    }));
+});
+
+rounds.on('roundStart', function(round, args){
+  var playerList = Array.prototype.slice.call(args[0])
+  fromArray(playerList)
+    .pipe(split())
+    .pipe(through2(function(chunk,_,cb){
+      if (chunk.length == 0){ return cb(); }
+      var playerName = getPlayerName(chunk.toString());
+      var player = world.find(playerName)[0];
+      console.log(player);
+      cb();
+    }));
+  if (round == 3){
+    rounds.setNext('gameOver');
+  } else{
+    rounds.setNext('roundStart');
+  }
+  rounds.next();
+})
+
+rounds.next();
