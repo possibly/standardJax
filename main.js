74490faf9b55238159ea445f2af0c2b54881be32
Tyler Brothers <tylerbrothers1@gmail.com>
2015-10-06 12:09:18 -0700

    Actions, stealing/sharing, fixed myWorld.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //my modules
 var World = require('./lib/World.js');
 var world = new World();
+var createAction = require('./lib/Action.js');
 
 //node modules
 var execFileSync = require('child_process').execFileSync;
@@ -13,9 +14,23 @@ var fs = require('fs');
 
 var programsDir = '/programs/';
 
-function getPlayerName(path){
+function nameFromPath(path){
   return path.split('/')[path.split('/').length-1];
 }
+
+rounds.on('setup', function(args){
+  fs.readdir(process.cwd()+programsDir, function(err, files){
+    var playerPaths = files.map(function(file){
+      return process.cwd()+programsDir+file;
+    })
+    playerPaths.forEach(function(playerPath){
+      execSync('chmod +x '+playerPath);
+      var playerName = nameFromPath(playerPath);
+      world.addPlayer(playerName);
+    });
+    rounds.next(playerPaths);
+  });
+});
 
 rounds.on('roundStart', function(round, args){
   var playerPaths = args[0];
@@ -28,28 +43,41 @@ rounds.on('roundStart', function(round, args){
   rounds.next(args[0]);
 });
 
-rounds.on('turnStart', function(round, turn, args){
-  var playerPath = args[0];
-  var playerName = getPlayerName(playerPath);
+function actionFromPath(playerPath){
+  var playerName = nameFromPath(playerPath);
   var player = world.getPlayer(playerName);
-  var playerOutput = execFileSync(playerPath, [], { input: JSON.stringify(player) });
-  var action = playerOutput.toString().trim('\n').split(' ');
-  if (action[0] === 'move'){
-    var oldCoords = player.getPosition();
-    var newCoords = player.move(action);
-    if (!world.collision(newCoords)){
-      world.remove(oldCoords[0], oldCoords[1]);
-      world.put(newCoords[0], newCoords[1], player);
-      console.log(player.name+' '+action+' successfully.');
-    }
-    else {
-      console.log(player.name+" can not "+action+" since "+world.get(newCoords[0], newCoords[1])+" is in the way.");
-    }
+  var playerInput = {'action': 'play', 'info': player.toString()};
+  var playerOutput = execFileSync(player.path, [], { input: JSON.stringify(playerInput) });
+  var action = createAction();
+  action.raw = playerOutput.toString().trim('\n').split(' ');
+  action.player = player;
+  return action
+}
+
+function interpret(action){
+  if ( action.raw[0] === 'move' ){
+    action.fn = world.move;
+  }
+  else if( action.raw[0] === 'steal' || action.raw[0] === 'share' ){
+    action.otherAction = actionFromPath(world.getPlayer(action.raw[1]).path);
+    if (action.otherAction.raw[0] !== 'steal' || action.otherAction.raw[0] !== 'share'){ action.otherAction.raw = ['steal']; }
+    action.fn = world.decide;
   }
   else{
-    console.log(action+' is not a valid action.');
+    throw new Error(action.raw+' is not a valid action.');
   }
-  console.log(JSON.stringify(player));
+
+  return action;
+}
+
+rounds.on('turnStart', function(round, turn, args){
+  var action = actionFromPath(args[0]);
+  try{
+    interpret(action).perform(world);
+  }catch(err){
+    console.log(err);
+  }
+  console.log(action.player.toString());
 });
 
 rounds.on('roundEnd', function(round, args){
@@ -60,15 +88,4 @@ rounds.on('gameOver', function(){
   console.log('game over');
 })
 
-fs.readdir(process.cwd()+programsDir, function(err, files){
-  var playerPaths = files.map(function(file){
-    return process.cwd()+programsDir+file;
-  })
-  playerPaths.forEach(function(playerPath){
-    execSync('chmod +x '+playerPath);
-    var playerName = getPlayerName(playerPath);
-    world.addPlayer(playerName);
-  });
-  rounds.setNext('roundStart');
-  rounds.next(playerPaths);
-});
+rounds.next();
